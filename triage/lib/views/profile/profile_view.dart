856e2efc569d1e8f2faec8245c6eaa956c3897d3
Asante Adarkwa Usman <asante.au96@gmail.com>
2021-12-06 22:04:01 +0000

    Worked on profile screen

--- triage/lib/views/profile/profile_view.dart
+++ triage/lib/views/profile/profile_view.dart
@@ -1,13 +1,156 @@
 import 'package:flutter/material.dart';
+import 'package:triage/custom_widgets/button/secondary_button.dart';
+import 'package:unicons/unicons.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Profile screen'),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80),
+        child: AppBar(
+          elevation: 0,
+          //leadingWidth: MediaQuery.of(context).size.width,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(10),
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: const Color.fromRGBO(85, 49, 78, 0.81),
+          title: const Text(
+            'Profile',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          const ProfileWidget(
+            iconName: UniconsLine.wallet,
+            text: 'Wallet',
+            onPressed: null,
+          ),
+          const Divider(
+            thickness: 1,
+            color: Color.fromRGBO(0, 0, 0, 0.1),
+            indent: 40,
+            endIndent: 40,
+          ),
+          const ProfileWidget(
+            iconName: UniconsLine.file_bookmark_alt,
+            text: 'Reviews',
+            onPressed: null,
+          ),
+          const Divider(
+            thickness: 1,
+            color: Color.fromRGBO(0, 0, 0, 0.1),
+            indent: 40,
+            endIndent: 40,
+          ),
+          const ProfileWidget(
+            iconName: UniconsLine.location_pin_alt,
+            text: 'Address',
+            onPressed: null,
+          ),
+          const Divider(
+            thickness: 1,
+            color: Color.fromRGBO(0, 0, 0, 0.1),
+            indent: 40,
+            endIndent: 40,
+          ),
+          const ProfileWidget(
+            iconName: UniconsLine.cog,
+            text: 'Profile Settings',
+            onPressed: null,
+          ),
+          const Divider(
+            thickness: 1,
+            color: Color.fromRGBO(0, 0, 0, 0.1),
+            indent: 40,
+            endIndent: 40,
+          ),
+          const ProfileWidget(
+            iconName: UniconsLine.envelope_open,
+            text: 'Contact Us',
+            onPressed: null,
+          ),
+          const Divider(
+            thickness: 1,
+            color: Color.fromRGBO(0, 0, 0, 0.1),
+            indent: 40,
+            endIndent: 40,
+          ),
+          const ProfileWidget(
+            iconName: UniconsLine.question,
+            text: 'About Us',
+            onPressed: null,
+          ),
+          const Divider(
+            thickness: 1,
+            color: Color.fromRGBO(0, 0, 0, 0.1),
+            indent: 40,
+            endIndent: 40,
+          ),
+          const SizedBox(height: 30),
+          SecondaryButton(
+            buttonText: 'Logout',
+            buttonColor: const Color.fromRGBO(206, 93, 29, 1),
+            onPressed: null,
+            width: MediaQuery.of(context).size.width * 0.25,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileWidget extends StatelessWidget {
+  final IconData? iconName;
+  final String? text;
+  final VoidCallback? onPressed;
+  const ProfileWidget(
+      {Key? key, required this.iconName, required this.text, this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SizedBox(
+        height: 45,
+        width: 45,
+        child: Card(
+          shape: const CircleBorder(),
+          color: const Color.fromRGBO(206, 93, 29, 1),
+          child: Icon(
+            iconName,
+            size: 28,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      title: Text(
+        text.toString(),
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w400,
+          fontFamily: 'Oxygen-Regular',
+          color: Color.fromRGBO(0, 0, 0, 0.7),
+        ),
+      ),
+      trailing: IconButton(
+        onPressed: onPressed,
+        icon: const Icon(
+          Icons.arrow_forward_ios,
+          size: 20,
+          color: Color.fromRGBO(0, 0, 0, 0.7),
+        ),
       ),
     );
   }
